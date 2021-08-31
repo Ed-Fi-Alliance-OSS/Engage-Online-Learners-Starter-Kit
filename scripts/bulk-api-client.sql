fe796443fb37f4405dd017981aeb55b74dd9f920
Stephen A. Fuqua <stephen@safnet.com>
2021-08-31 17:03:47 -0500

    Update script to accept key & secret as vars

--- scripts/bulk-api-client.sql
+++ scripts/bulk-api-client.sql
@@ -57,8 +57,8 @@ insert into dbo.ApiClients (
   Application_ApplicationId
 )
 select
-  'consoleBulkLoader',
-  'consoleBulkLoaderSecret',
+  $(ClientKey),
+  $(ClientSecret),
   'Client Bulk Loader',
   1,
   0,
